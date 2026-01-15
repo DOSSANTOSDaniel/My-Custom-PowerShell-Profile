4eaa0951d34979dd1e0cb032fa66d392f66d640e
DOS SANTOS <dossantosjdf@gmail.com>
2026-01-15 19:05:53 +0100

    Delete install/functions/test_is_admin.ps1

--- install/functions/test_is_admin.ps1
+++
@@ -1,6 +0,0 @@
-# Vérifier si l'utilisateur et en mode administrateur 
-function Test-IsAdmin {
-    ([Security.Principal.WindowsPrincipal] `
-        [Security.Principal.WindowsIdentity]::GetCurrent()
-    ).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
-}
