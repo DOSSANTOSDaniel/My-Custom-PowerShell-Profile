c13f8051c53984142da83b8682a07981ac7370bd
DOS SANTOS <dossantosjdf@gmail.com>
2026-01-17 10:37:13 +0100

    Refactor PowerShell Git auto-updater script

--- scripts/updater.ps1
+++ scripts/updater.ps1
@@ -151,7 +151,7 @@ if ($local -and $remote -and $local -ne $remote) {
         # Ouvrir une nouvelle fenêtre WT et recharger le profil
         $commands = @(
             '. $PROFILE'
-            'Write-Host "`n -- Journal de mise à jour -- " -ForegroundColor Cyan'
+            '& { Write-Host "`n-- Journal de mise à jour --" -ForegroundColor Cyan }'
             'Get-Content $LogFile -Tail 20'
         )
 
@@ -178,4 +178,5 @@ else {
     Log_msg "Pas de mise à jour !."
 
 }
+
 
